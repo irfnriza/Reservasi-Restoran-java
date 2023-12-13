1c4d4fa78005a0504e823f589ed16f23df989bcc
aufazaikra <128202887+aufazaikra@users.noreply.github.com>
2023-12-13 14:12:01 +0700

    Update MenuRestoran2.java

--- src/menurestoran/MenuRestoran2.java
+++ src/menurestoran/MenuRestoran2.java
@@ -146,27 +146,39 @@ public class MenuRestoran2 extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        saveOrderInfoToFile("meja 1");
+        this.setVisible(false);
+        new reservationDate().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        saveOrderInfoToFile("meja 2");
+        this.setVisible(false);
+        new reservationDate().setVisible(true);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        saveOrderInfoToFile("meja 3");
+        this.setVisible(false);
+        new reservationDate().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        saveOrderInfoToFile("meja 4");
+        this.setVisible(false);
+        new reservationDate().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        // TODO add your handling code here:
+        saveOrderInfoToFile("meja 5");
+        this.setVisible(false);
+        new reservationDate().setVisible(true);
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        // TODO add your handling code here:
+        saveOrderInfoToFile("meja 6");
+        this.setVisible(false);
+        new reservationDate().setVisible(true);
     }//GEN-LAST:event_jButton6ActionPerformed
 
     /**
